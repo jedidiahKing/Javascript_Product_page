206bf0a0b9ecf6824356253502de064a174ab12c
MELISSA <pc@MELISSA.com>
2021-04-12 11:11:58 +0000

    Adding javascript

---
+++ store.js
@@ -0,0 +1,65 @@
+const html = `
+
+<section class="container content-section">
+            <h2 class="section-header">MY SHOP</h2>
+            <div class="shop-items">
+                <div class="shop-item">
+                    <span class="shop-item-title">Barcelona Jersey</span>
+                    <img class="shop-item-image" src="Images/home-jersey.jpg">
+                    <div class="shop-item-details">
+                        <span class="shop-item-details">$12.99</span>
+                        <button class="btn btn-primary shop-item-button" type="button">ADD TO CART</button>
+                    </div>
+                </div>
+                <div class="shop-item">
+                    <span class="shop-item-title">Iphone 12</span>
+                    <img class="shop-item-image" src="Images/iphone.png">
+                    <div class="shop-item-details">
+                        <span class="shop-item-price">$14.99</span>
+                        <button class="btn btn-primary shop-item-button"type="button">ADD TO CART</button>
+                    </div>
+                </div>
+                <div class="shop-item">
+                    <span class="shop-item-title">Nike Air Force</span>
+                    <img class="shop-item-image" src="Images/nike-air.jpg">
+                    <div class="shop-item-details">
+                        <span class="shop-item-price">$9.99</span>
+                        <button class="btn btn-primary shop-item-button" type="button">ADD TO CART</button>
+                    </div>
+                </div>
+                <div class="shop-item">
+                    <span class="shop-item-title">Beats Speaker</span>
+                    <img class="shop-item-image" src="Images/speaker.jpg">
+                    <div class="shop-item-details">
+                        <span class="shop-item-price">$19.99</span>
+                        <button class="btn btn-primary shop-item-button" type="button">ADD TO CART</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="container content-section">
+            <h2 class="section-header">SHOPPING CART</h2>
+            <div class="cart-row">
+                <span class="cart-item cart-header cart-column">ITEM</span>
+                <span class="cart-price cart-header cart-column">PRICE</span>
+                <span class="cart-quantity cart-header cart-column">QUANTITY</span>
+            </div>
+            <div class="cart-items">
+            </div>
+            <div class="cart-total">
+                <strong class="cart-total-title">Total</strong>
+                <span class="cart-total-price">$0</span>
+            </div>
+            <button class="btn btn-primary btn-purchase" type="button">PURCHASE</button>
+        </section>
+
+`
+
+document.querySelector('#main').insertAdjacentHTML('afterbegin', html)
+
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+    
+}
